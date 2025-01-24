26bca839ea3f2e66a17c3b4578e2c8014cf2dd33
billgo <cocobill@vip.qq.com>
2025-01-24 09:52:00 +0800

    update

--- packages/breakpoints/tests/index.test.tsx
+++ packages/breakpoints/tests/index.test.tsx
@@ -1,42 +1,5 @@
-import { afterEach, beforeEach, expect, test } from "bun:test";
-import assert from "node:assert";
-import React, { act } from "react";
-import { createRoot } from "react-dom/client";
+import { expect, test } from "bun:test";
 
-const App = () => {
-  return (
-    <div role="main">
-      <article>
-        <h3>Welcome to Bun!</h3>
-      </article>
-    </div>
-  );
-};
-
-let container: HTMLDivElement | null = null;
-
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  if (container) {
-    document.body.removeChild(container);
-    container = null;
-  }
-});
-
-test("App", () => {
-  act(() => {
-    createRoot(container!).render(<App />);
-  });
-
-  expect(container?.children[0].innerHTML).toMatchSnapshot();
-  assert(container);
-
-  const article = container!.querySelector("article");
-  expect(article?.children.length).toBe(1);
+test("tutorial: 2 + 2", () => {
+  expect(2 + 2).toBe(4);
 });
